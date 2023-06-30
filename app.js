d5b94c5e39bbded39c96fb28ca6a5e5a4fedcffc
unknown <jeromesloss@yahoo.com>
2023-06-30 11:03:48 -0400

    minimal styling and psuedo code added

--- app.js
+++ app.js
@@ -1,3 +1,52 @@
+const choiceBtn = document.getElementById('choice');
+
+choiceBtn.addEventListener('click', () => makeChoice());
+
+// Create player
+class player {
+    constructor (name, job) {
+        this.name = name;
+        this.job = job;
+    }
+}
+
+// Create Music Label
+class villian {
+    constructor (name, job) {
+        this.name = name;
+        this.job = job;
+    }
+}
+
+
+// Add choices for level 1
+
+
+
+
+
+
+// Add choices for level 2
+
+
+
+
+
+
+// Add choices for level 3
+
+
+
+
+
+// Add a way to record choices that the player makes
+
+
+
+
+
+// Show the result from the choices made
+
 
 
 
